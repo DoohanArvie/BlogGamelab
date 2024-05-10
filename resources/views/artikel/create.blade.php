269554494583a82fc63bf55a67014ef6e20a8a55
d00h4n <doohan.mintarso@gmail.com>
2024-05-10 10:48:35 +0700

    Create Artikel v 1.0

--- resources/views/artikel/create.blade.php
+++ resources/views/artikel/create.blade.php
@@ -7,12 +7,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Create Artikel</h1>
+            <h1 class="m-0">New Member</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Create Artikel</li>
+              <li class="breadcrumb-item active">Add New Member</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -22,75 +22,82 @@
 
     <!-- Main content -->
     <div class="content">
-        <div class="container mt-5">
-            <div class="card border-0 shadow-sm rounded">
-                <div class="card-body">
-                    <form action="{{ route('storeArtikel') }}" method="POST" enctype="multipart/form-data">
-    
-                        @csrf
-                        <div class="form-group">
-                            <label for="deskripsi">Nama Kategori</label>
-                            <select class="form-control" name="id_kategori" id="id_kategori" required="required">
-                                @foreach ($kategoris as $kategori)
-                                    <option value="{{ $kategori->id }}">{{ $kategori->deskripsi }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label class="font-weight-bold">GAMBAR</label>
-                            <input type="file" class="form-control @error('gambar') is-invalid @enderror"
-                                name="gambar">
-    
-                            <!-- error message untuk title -->
-                            @error('gambar')
-                                <div class="alert alert-danger mt-2">
-                                    {{ $message }}
+         <div class="container mt-5">
+            <div class="content">
+                <div class="container mt-5">
+                    <div class="card border-0 shadow-sm rounded">
+                        <div class="card-body">
+                            <form action="{{ route('storeArtikel') }}" method="POST" enctype="multipart/form-data">
+            
+                                @csrf
+                                <div class="form-group">
+                                    <label for="deskripsi">Team</label>
+                                    <select class="form-control" name="id_kategori" id="id_kategori" required="required">
+                                        @foreach ($kategoris as $kategori)
+                                            <option value="{{ $kategori->id }}">{{ $kategori->deskripsi }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                            @enderror
-                        </div>
-    
-                        <div class="form-group">
-                            <label class="font-weight-bold">JUDUL</label>
-                            <input type="text" class="form-control @error('judul') is-invalid @enderror"
-                                name="judul" value="{{ old('judul') }}" placeholder="Masukkan Judul Artikel">
-    
-                            <!-- error message untuk title -->
-                            @error('judul')
-                                <div class="alert alert-danger mt-2">
-                                    {{ $message }}
+                                <div class="form-group">
+                                    <label class="font-weight-bold">Image</label>
+                                    <input type="file" class="form-control @error('gambar') is-invalid @enderror"
+                                        name="gambar">
+            
+                                    <!-- error message untuk title -->
+                                    @error('gambar')
+                                        <div class="alert alert-danger mt-2">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                        </div>
-    
-                        <div class="form-group">
-                            <label class="font-weight-bold">KONTEN</label>
-                            <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi" rows="5"
-                                placeholder="Masukkan deskripsi Artikel">{{ old('deskripsi') }}</textarea>
-    
-                            <!-- error message untuk content -->
-                            @error('deskripsi')
-                                <div class="alert alert-danger mt-2">
-                                    {{ $message }}
+            
+                                <div class="form-group">
+                                    <label class="font-weight-bold">Name</label>
+                                    <input type="text" class="form-control @error('judul') is-invalid @enderror"
+                                        name="judul" value="{{ old('judul') }}" placeholder="Masukkan Judul Artikel">
+            
+                                    <!-- error message untuk title -->
+                                    @error('judul')
+                                        <div class="alert alert-danger mt-2">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
+            
+                                <div class="form-group">
+                                    <label class="font-weight-bold">Bio</label>
+                                    <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi" rows="5"
+                                        placeholder="Masukkan deskripsi Artikel">{{ old('deskripsi') }}</textarea>
+            
+                                    <!-- error message untuk content -->
+                                    @error('deskripsi')
+                                        <div class="alert alert-danger mt-2">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+            
+                                <button type="submit" class="btn btn-md btn-primary">Save</button>
+                                <button type="reset" class="btn btn-md btn-warning">Reset</button>
+            
+                            </form>
                         </div>
-    
-                        <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
-                        <button type="reset" class="btn btn-md btn-warning">RESET</button>
-    
-                    </form>
+                    </div>
                 </div>
             </div>
+          </div>
+          <!-- /.col-md-6 -->
         </div>
+        <!-- /.row -->
+      </div><!-- /.container -->
     </div>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
      
   <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
-  <script>
-      CKEDITOR.replace( 'deskripsi' );
-      CKEDITOR.config.autopParagraph = false;
-  </script>
-
+<script>
+    CKEDITOR.replace( 'deskripsi' );
+    CKEDITOR.config.autoParagraph = false;
+    </script>
 @endsection
